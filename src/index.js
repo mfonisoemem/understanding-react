e9485999e5b2eaf620fd4e342b9bd1e6c5d3c0a0
Mfoniso Emem <99399506+mfonisoemem@users.noreply.github.com>
2023-11-15 13:21:31 +0100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDom from "react-dom";
+ReactDom.render(
+<div>
+  <h1> Hello World, I am testing my react challenge </h1>
+  <ul>
+    <li>I love food </li>
+    <li> I love cats</li>
+    <li> I love money</li>
+  </ul>
+  <p> Anyways, I believe that this is enough to show my coding skills</p>
+</div>,
+document.getElementById("root")
+  );
+//It should display a h1 heading.
+//It should display an unordered list (bullet points).
+//It should contain 3 list elements.
